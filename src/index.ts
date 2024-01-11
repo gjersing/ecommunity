e2b973c5dd7aad8f2a7d45927d88c4833a31e9a6
gjersing <csgjersing@gmail.com>
2024-01-11 15:01:50 -0800

    Initial server - express / apollo / graphQL

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import { MikroORM } from '@mikro-orm/postgresql';
-import { Post } from './entities/Post';
+// import { Post } from './entities/Post';
 import mikroOrmConfig from './mikro-orm.config';
+import express from 'express';
+import { __port__ } from './constants';
 
 const main = async () => {
-    // Initialize MikroORM and migrate
-    const orm = await MikroORM.init(mikroOrmConfig);
-    await orm.getMigrator().up();
-    
-    // Fork to use EM methods & test creating Posts
-    const fork = orm.em.fork();
-    const post = fork.create(Post, {title: 'my first post'});
-    await fork.persistAndFlush(post);
+  // Initialize MikroORM and migrate
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  app.get('/', (_, res) => {
+    res.send("hello");
+  });
+
+  app.listen(__port__, () => {
+    console.log(`Server started successfully on localhost:${__port__}`);
+  });
+  
+  // Fork to use EM methods & test creating Posts
+  // const fork = orm.em.fork();
+  // const post = fork.create(Post, {title: 'my first post'});
+  // await fork.persistAndFlush(post);
+
+  // const posts = await fork.find(Post, {});
+  // console.log(posts);
 };
 
 main().catch((err) => {
