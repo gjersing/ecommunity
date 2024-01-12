74e86c6369a29c4cff7eaa69b539a3cc143d07d1
gjersing <csgjersing@gmail.com>
2024-01-12 13:58:33 -0800

    GraphQL Post create/find resolver

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, Opt, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType() // Identifies class to GraphQL
 @Entity()
 export class Post {
+  @Field(() => Int) // Optional: Exposes property to GraphQL
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property()
   createdAt: Date & Opt = new Date();
 
+  @Field(() => String)
   @Property({ onUpdate: () => new Date() })
   updatedAt: Date & Opt = new Date();
 
+  @Field()
   @Property({ type: "text" })
   title: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,36 @@
-import { MikroORM } from '@mikro-orm/postgresql';
-// import { Post } from './entities/Post';
-import mikroOrmConfig from './mikro-orm.config';
-import express from 'express';
-import { __port__ } from './constants';
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/postgresql";
+import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { __port__ } from "./constants";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
-  // Initialize MikroORM and migrate
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
+  const fork = orm.em.fork();
 
   const app = express();
 
-  app.get('/', (_, res) => {
-    res.send("hello");
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: fork }),
   });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
   app.listen(__port__, () => {
     console.log(`Server started successfully on localhost:${__port__}`);
   });
-  
-  // Fork to use EM methods & test creating Posts
-  // const fork = orm.em.fork();
-  // const post = fork.create(Post, {title: 'my first post'});
-  // await fork.persistAndFlush(post);
-
-  // const posts = await fork.find(Post, {});
-  // console.log(posts);
 };
 
 main().catch((err) => {
-    console.error(err);
+  console.error(err);
 });
