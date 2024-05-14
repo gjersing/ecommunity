0407e1000c0f307fe206fa41be0170faad4809d1
gjersing <csgjersing@gmail.com>
2024-05-14 16:42:32 -0700

    Add ecommunity.us to CORS_ORIGIN for prod

--- src/utils/createLikeStatusLoader.ts
+++ src/utils/createLikeStatusLoader.ts
@@ -6,21 +6,15 @@ export const createLikeStatusLoader = () =>
     async (keys) => {
       const likes = await Like.findBy(keys as any);
 
-      console.log("inside loader");
-      console.log(likes);
-
       const likeIdsToLike: Record<string, Like> = {};
       likes.forEach((like) => {
         likeIdsToLike[`${like.userId}|${like.postId}`] = like;
       });
 
-      console.log(likeIdsToLike);
-
       const returnVal = keys.map(
         (key) => likeIdsToLike[`${key.userId}|${key.postId}`],
       );
 
-      console.log(returnVal);
       return returnVal;
     },
   );
